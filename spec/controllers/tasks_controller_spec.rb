d060b308a5bbfd046e81f71a05ee82d45548aa9f
JarfigNewton <jaredschil@gmail.com>
2017-11-15 04:07:59 +0000

    Tasks display in correct order after they've been updated

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,12 +5,26 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
+
       get :index
 
       expect(response).to have_http_status :success
 
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
+    end
+
+    it "should list the tasks in the correct order after updating" do
+      task1 = FactoryBot.create(:task)
+      task2 = FactoryBot.create(:task)
+      task1.update_attributes(title: "Something else")
+      get :index
+
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      response_ids = response_value.collect do |task|
+        task["id"]
+      end
+      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
 
